1792418eae63687e543529cab1e7a175ca1e2199
amorusoriccardo <amoruso.riccardo@iisgalvanimi.edu.it>
2019-11-06 10:44:43 +0000

    02.1

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,16 +1,19 @@
 
 
-var express = require('express');
-var app = express();
-app.set('view engine', 'pug');
+const express = require('express');
+const people = require('./people.json'); //Copia il file people.json dentro la variabile people
+const app = express();
 
+app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-     res.render('index');
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles //Passa il vettore profiles alla pagina index.pug
+  });
 });
 
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
+});
